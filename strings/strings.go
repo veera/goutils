2eba96922e40e7358b39712fa6f22c7fb5b832eb
Veera <veerasekar.v.g@gmail.com>
2025-04-19 21:52:59 +1000

    string reverse

--- strings/strings.go
+++ strings/strings.go
@@ -24,3 +24,11 @@ func SnakeCase(s string) string {
 	s = re.ReplaceAllString(strings.TrimSpace(s), "_")
 	return strings.ToLower(s)
 }
+
+func Reverse(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
--- strings/strings_test.go
+++ strings/strings_test.go
@@ -48,3 +48,21 @@ func TestSnakeCase(t *testing.T) {
 		require.Equal(t, tt.want, SnakeCase(tt.input))
 	}
 }
+
+func TestReverse(t *testing.T) {
+	tests := []struct {
+		input string
+		want  string
+	}{
+		{"hello", "olleh"},
+		{"Go", "oG"},
+		{"", ""},
+		{"a", "a"},
+		{"racecar", "racecar"},
+		{"123 💡 abc", "cba 💡 321"},
+	}
+
+	for _, tt := range tests {
+		require.Equal(t, tt.want, Reverse(tt.input), "Reverse(%q)", tt.input)
+	}
+}
